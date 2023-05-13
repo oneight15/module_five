55aac115c4a71f6fd8c9c7afb751e50ce46043aa
oneight15 <oneight1508@gmail.com>
2023-05-13 23:06:43 +0500

    fix: correct solution

--- js/main.js
+++ js/main.js
@@ -2,18 +2,25 @@
 
 const ads = document.querySelector('.ads');
 
-ads.remove();
-
 // получаем карточки
 const thirdCard = document.querySelector('.item_three');
 const fourthCard = document.querySelector('.item_four');
 const fifthCard = document.querySelector('.item_five');
 
-thirdCard.after(fourthCard);
-
 // получаем контент-контейнер
 const thirdCardContainer = thirdCard.querySelector('.content');
 const fifthCardContainer = fifthCard.querySelector('.content');
+
+// получаем заголовки карточек
+const firstTitle = document.querySelector('.item_one h2');
+const secondTitle = document.querySelector('.item_two .item__title');
+const thirdTitle = document.querySelector('.item_three .item__title');
+const fifthTitle = document.querySelector('.item_five .item__title');
+const sixthTitle = document.querySelector('.item_six .item__title');
+
+// создаем третий заголовок и текст для него
+const textThirdTitle = document.createTextNode('This и прототипы объектов');
+const correctThirdTitle = thirdTitle.cloneNode();
 
 // получаем списки карточек
 const secondPropsList = document.querySelector('.item_two .props__list');
@@ -22,15 +29,28 @@ const fourthPropsList = document.querySelector('.item_four .props__list');
 const fifthPropsList = document.querySelector('.item_five .props__list');
 const sixthPropsList = document.querySelector('.item_six .props__list');
 
-thirdCardContainer.append(fifthPropsList);
-fifthCardContainer.append(thirdPropsList);
-
 // получаем элементы списков карточек
 const propsItemsTwo = secondPropsList.querySelectorAll('.props__item');
 const propsItemsFour = fourthPropsList.querySelectorAll('.props__item');
 const propsItemsSix = sixthPropsList.querySelectorAll('.props__item');
 
+ads.remove();
+
+thirdCard.after(fourthCard);
+
+thirdCardContainer.append(fifthPropsList);
+fifthCardContainer.append(thirdPropsList);
+
 propsItemsFour[2].after(propsItemsTwo[3]);
 secondPropsList.append(propsItemsSix[8]);
 secondPropsList.append(propsItemsSix[9]);
 
+secondPropsList.before(fifthTitle);
+sixthPropsList.before(secondTitle);
+thirdPropsList.before(sixthTitle);
+thirdTitle.remove();
+
+correctThirdTitle.append(textThirdTitle);
+fifthPropsList.before(correctThirdTitle);
+
+firstTitle.classList.add('item__title');
